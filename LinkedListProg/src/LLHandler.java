bcc0a808ae2005a676de507bac02f966bdb7a77f
AdityaPNM98 <aditya.nambiar24@gmail.com>
2019-07-13 20:01:22 +0530

    Added Java Files

---
+++ LinkedListProg/src/LLHandler.java
@@ -0,0 +1,19 @@
+
+public class Node{
+	Object data = null;
+	Node next = null;
+	
+	public Node Node(Object data, Node next) {
+		this.data = data;
+		this.next = next;
+	}
+	
+}
+public class LinkedList{
+	
+}
+public class LLHandler {
+	public static void main(String args[]) {
+		// Create Nodes and Perform operation on Linked List here.
+	}
+}
---
+++ PractiseJava/src/PractiseProb2.java
@@ -0,0 +1,9 @@
+
+public class PractiseProb2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
